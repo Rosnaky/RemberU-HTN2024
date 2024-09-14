627d27744d42ac6d5a092cc27fe3ba9d187574d5
Ronak Patel <98669069+Rosnaky@users.noreply.github.com>
2024-09-14 14:13:52 -0400

    added layout builder for card

--- lib/widgets/user_card.dart
+++ lib/widgets/user_card.dart
@@ -24,27 +24,28 @@ class _UserCardState extends State<UserCard> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Card(
-      child: FutureBuilder<FirebaseUser?>(
-          future: userFuture,
-          builder: (contex, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else if (snapshot.hasData) {
-              final user = snapshot.data;
-              return Column(
-                children: [
-                  Text(user?.firstName ?? 'No First Name'),
-                  Text(user?.email ?? 'No Email'),
-                ],
-              );
-            } else {
-              return Text('No user data available');
-            }
-          }),
-    ));
+    return Center(child: LayoutBuilder(builder: (context, constraints) {
+      return Card(
+        child: FutureBuilder<FirebaseUser?>(
+            future: userFuture,
+            builder: (contex, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else if (snapshot.hasData) {
+                final user = snapshot.data;
+                return Column(
+                  children: [
+                    Text(user?.firstName ?? 'No First Name'),
+                    Text(user?.email ?? 'No Email'),
+                  ],
+                );
+              } else {
+                return Text('No user data available');
+              }
+            }),
+      );
+    }));
   }
 }
